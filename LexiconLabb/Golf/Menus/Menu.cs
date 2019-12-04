89c585326e7594dfc62029f82ad1f2a364e9a47e
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-04 13:46:16 +0100

    Set up the Menu class

--- LexiconLabb/Golf/Menus/Menu.cs
+++ LexiconLabb/Golf/Menus/Menu.cs
@@ -6,5 +6,12 @@ namespace Golf.Menus
 {
     class Menu
     {
+        public List<string> Elements { get; set; }
+        public int Button { get; set; }
+
+        public Menu()
+        {
+            Elements = new List<string>();
+        }
     }
 }

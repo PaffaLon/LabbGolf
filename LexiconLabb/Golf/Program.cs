2fb2450c1354579283d499a0725053ff007add2a
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-04 13:21:31 +0100

    Main clean up

--- LexiconLabb/Golf/Program.cs
+++ LexiconLabb/Golf/Program.cs
@@ -4,9 +4,9 @@ namespace Golf
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }

181e3501d36f98c0ff685fad42ea062a2094cff6
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-04 15:55:31 +0100

    new classes with light logic

---
+++ LexiconLabb/Golf/Characters/Character.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.Characters
+{
+    public class Character
+    {
+        string Name { get; set; }
+        int Strength { get; set; }   
+    }
+}
---
+++ LexiconLabb/Golf/Characters/Players/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.Characters.Players
+{
+    public class Player : Character
+    {
+        
+        public bool Direction { get; set; }
+
+    }
+}

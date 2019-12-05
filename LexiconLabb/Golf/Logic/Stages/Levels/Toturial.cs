9088ca7b16fb5f3edf7d75462020c962556a7fe4
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-05 14:36:12 +0100

    Level toturial don by issue

--- LexiconLabb/Golf/Logic/Stages/Levels/Toturial.cs
+++ LexiconLabb/Golf/Logic/Stages/Levels/Toturial.cs
@@ -6,5 +6,31 @@ namespace Golf.Logic.Stages.Levels
 {
     public class Toturial : Level
     {
+
+        public string GetLevelName(string levelName)
+        {
+            this.Name = ("Toturial");
+            return this.Name;
+        }
+
+        public void LevelDefaultSettings()
+        {
+            GetDifficulity();
+            GetObjectStartingPositions();
+
+            void GetObjectStartingPositions()
+            {
+                this.StartingPositionGolfHole = 0;
+                this.StartinPositionGolfBall = 35;
+                this.StartingPostionPlayer = this.StartinPositionGolfBall - 1;
+            }
+
+            int GetDifficulity()
+            {
+                this.Difficulity = (int)DifficulityRatings.Beginer;
+                return this.Difficulity;
+            }
+        }
+
     }
 }

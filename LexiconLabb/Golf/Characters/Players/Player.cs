06b51bb13671fe4e98cd8509a254e58dde776bcb
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-05 16:00:49 +0100

    commit

--- LexiconLabb/Golf/Characters/Players/Player.cs
+++ LexiconLabb/Golf/Characters/Players/Player.cs
@@ -6,8 +6,15 @@ namespace Golf.Characters.Players
 {
     public class Player : Character
     {
-        
+        public int MaxSwingStrength { get; set; }
+        public int MinSwingStrength { get; set; }
         public bool Direction { get; set; }
 
+        public Player()
+        {
+            MinSwingStrength = 1;
+            MaxSwingStrength = 20;
+
+        }
     }
 }

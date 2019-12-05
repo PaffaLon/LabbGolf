cd44cab1ecaba2839698c6b3b15d79773048bf6f
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-05 09:59:43 +0100

    Scoreboard menu don.

---
+++ LexiconLabb/Golf/Menus/Content/Scoreboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.Menus.Content
+{
+    public class Scoreboard
+    {
+
+        public string[] getMenuItems = new string[0];
+        public void Content(int menuButton)
+        {
+            string[] unselectedMenuItems = new string[0];
+            string[] selectedMenuItems = new string[0];
+
+            unselectedMenuItems[0] = ("Exit");
+            selectedMenuItems[0] = (">> Exit <<");
+
+            if(menuButton == 0)
+                getMenuItems[0] = unselectedMenuItems[0];
+
+            if (menuButton == 1)
+                getMenuItems[0] = selectedMenuItems[0];
+        }
+        public void GetScoreboard()
+        {
+
+        }
+    }
+}

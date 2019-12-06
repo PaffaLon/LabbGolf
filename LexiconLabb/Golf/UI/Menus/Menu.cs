756192ef69f8503c7f972737fbbba989fdbdc39a
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-06 15:20:51 +0100

    Added a UI folder and placed all menu conetent inside the UI folder.

---
+++ LexiconLabb/Golf/UI/Menus/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.UI.Menus
+{
+    public class Menu
+    {
+        public List<string> Elements { get; set; }
+        public int Button { get; set; }
+
+        public Menu()
+        {
+            Elements = new List<string>();
+        }
+    }
+}

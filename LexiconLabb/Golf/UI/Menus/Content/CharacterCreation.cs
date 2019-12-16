52a88ff58ab578bb4d2825bcf49d2e4a98ce6bc5
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-16 15:54:00 +0100

    commit

---
+++ LexiconLabb/Golf/UI/Menus/Content/CharacterCreation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.UI.Menus.Content
+{
+    public class CharacterCreation
+    {
+        public string[] GetComponents = new string[1];
+        public void Content()
+        {
+            string[] items = new string[];    
+            
+        }
+    }
+}

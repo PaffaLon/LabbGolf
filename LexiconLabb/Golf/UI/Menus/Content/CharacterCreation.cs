f52f680683b4078d9f32f1495c737b745394756f
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-17 10:46:14 +0100

    Set up the basic functionallity for creating a new character.

--- LexiconLabb/Golf/UI/Menus/Content/CharacterCreation.cs
+++ LexiconLabb/Golf/UI/Menus/Content/CharacterCreation.cs
@@ -9,8 +9,9 @@ namespace Golf.UI.Menus.Content
         public string[] GetComponents = new string[1];
         public void Content()
         {
-            string[] items = new string[];    
-            
+            string[] items = new string[0];
+            items[0] = ("Enter Player Name: ");
+            GetComponents[0] = items[0];
         }
     }
 }

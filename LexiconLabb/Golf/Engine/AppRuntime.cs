50f355f6aa98c1cbfc322730b0434c1858d66051
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-17 11:02:45 +0100

    Started working on some kind of application engine. To use as the most underlaying component of the application. In order to both keep track of and execture diffrent task that has to run in a specefic order. Or simultatiously. A task can be running a menu, sending webb data. Close the application with more.
    The goal is to have a logical source point to prevent an ininfit amount of recursion happning. Bowth with rfering to anoter class and craeting an instance of a class to a mini mum.

---
+++ LexiconLabb/Golf/Engine/AppRuntime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.Engine
+{
+    /// <summary>
+    /// The AppRuntime class determents where the user are located in the application.
+    /// Which segemts of code to run, stand alone or simultatnious.
+    /// </summary>
+    public class AppRuntime
+    {
+        private bool Running { get; set; }
+        AppRuntime()
+        {
+            Running = true;
+        }
+
+        public void RunTime()
+        {
+            while(Running == true)
+            {
+                
+            }
+        }
+    }
+}

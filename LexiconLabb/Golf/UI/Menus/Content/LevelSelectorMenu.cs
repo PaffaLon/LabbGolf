e76e749419437b306fd3ce792092d3104153151d
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-19 15:48:11 +0100

    Added a class that will work as the selection menu for levels. Or level browser.

---
+++ LexiconLabb/Golf/UI/Menus/Content/LevelSelectorMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.UI.Menus.Content
+{
+    public class LevelSelectorMenu
+    {
+        public void Content()
+        {
+            //GetLevelNames
+
+        }
+    }
+}

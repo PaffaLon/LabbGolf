842533aeeb5f35a5b83ce7c3802c1da746da3862
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-21 12:16:02 +0100

    Removed unessery commented code, that no longer is needed for testing a functionallity directly to the menu.

--- LexiconLabb/Golf/Program.cs
+++ LexiconLabb/Golf/Program.cs
@@ -8,11 +8,6 @@ namespace Golf
     {
         static void Main()
         {
-            /*  --The comented code is left for testing.
-            MenuManager menuManager = new MenuManager();
-            menuManager.GetMenu();
-            */
-
             /***    The Engine is still in an experimental mode.    ****/
             AppEngine appRuntime = new AppEngine();
             appRuntime.RunTime();

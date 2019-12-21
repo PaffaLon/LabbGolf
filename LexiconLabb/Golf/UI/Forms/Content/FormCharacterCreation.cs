e348a2ddb262f79df684ea9c14bb6d9db22d4a98
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-21 12:01:21 +0100

    Setting up a form that will manage the character creation.

---
+++ LexiconLabb/Golf/UI/Forms/Content/FormCharacterCreation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.UI.Forms.Content
+{
+    public class FormCharacterCreation : Form
+    {
+        public enum Buttons
+        {
+            Save,
+            Exit
+        }
+    }
+}
---
+++ LexiconLabb/Golf/UI/Forms/Form.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.UI.Forms
+{
+    public class Form
+    {
+        public List<string> Elements { get; set;}
+        public int Button { get; set; }
+
+        public Form()
+        {
+            Elements = new List<string>();
+        }
+    }
+}

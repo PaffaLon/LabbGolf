6a54d6e643cf4912b7726243896ad1e86cee54fc
Tobias Olsson <tobiasbjornolsson@gmail.com>
2019-12-22 18:16:22 +0100

    Added a key tracker method for testing purpose.

--- LexiconLabb/Golf/Program.cs
+++ LexiconLabb/Golf/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Golf.Engine;
+using System;
 
 namespace Golf
 {
@@ -7,8 +8,18 @@ namespace Golf
         static void Main()
         {
             /***    The Engine is still in an experimental mode.    ****/
+            TrackKeyPress();
             AppEngine appRuntime = new AppEngine();
             appRuntime.RunTime();
+        }
+
+        static void TrackKeyPress()
+        {
+            ConsoleKeyInfo cki;
+            cki = Console.ReadKey();
+
+            Console.WriteLine(cki.Key.GetHashCode().ToString());
+            Console.ReadKey();
         }
     }
 }

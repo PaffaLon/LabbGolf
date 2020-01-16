bc1561db295ab0a0bc772981642952692f54b0e9
Tobias Olsson <tobiasbjornolsson@gmail.com>
2020-01-16 15:22:55 +0100

    Changed the name of the class from "LevelSelector.cs" to "LevelBrowser.cs".
    Reworking the logic for browsing levels in the UI.
    
    Adding logic for making the menu unic and representive in the engine and scope of the UI.

---
+++ LexiconLabb/Golf/UI/Menus/Content/LevelBrowser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Golf.UI.Menus.Content
+{
+    public class LevelBrowser : Menu
+    {
+        public enum Buttons
+        {
+            Exit
+        }
+
+        public LevelBrowser()
+        {
+            if (this.DefaultValuesSet == false)
+                SetDefaultValues();
+        }
+
+        private void SetDefaultValues()
+        {
+                if (this.DefaultValuesSet)
+                this.ID = ("LevelBrowser");
+
+            this.ActiveButton = (int)Buttons.Exit;
+            this.DefaultValuesSet = true;
+        }
+
+        public void Content()
+        {
+            //GetLevelNames
+            string[] levelNames = new string[2];
+            levelNames[0] = ("Totuerial");
+            levelNames[1] = ("");
+            levelNames[2] = ("");
+        }
+    }
+}

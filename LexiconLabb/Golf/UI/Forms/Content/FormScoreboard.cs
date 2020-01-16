b51f28fa53efefa945e899b8c48d4aacd8a64de1
Tobias Olsson <tobiasbjornolsson@gmail.com>
2020-01-16 15:20:18 +0100

    Setting up the default logic for the scorboard object to be handaled as a ´n losely coupeld object in the engine from the scope of the UI.

--- LexiconLabb/Golf/UI/Forms/Content/FormScoreboard.cs
+++ LexiconLabb/Golf/UI/Forms/Content/FormScoreboard.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Golf.UI.Forms.Content
 {
-    public class FormScoreboard
+    public class FormScoreboard : Form, IForm
     {
         public void GetScoreboard()
         {

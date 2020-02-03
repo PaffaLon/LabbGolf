7a746138cbcfa033117cc12890b0c48f38da8486
Tobias Olsson <tobiasbjornolsson@gmail.com>
2020-02-03 10:29:37 +0100

    Added a new projekt

---
+++ LexiconLabb/GameLogic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GameLogic
+{
+    class Program
+    {
+        private static bool _running;
+        static void Main()
+        {
+            ConsoleKeyInfo cki;
+            cki = Console.ReadKey();
+
+            Console.WriteLine(cki.Key.GetHashCode());
+
+            //Up: 38
+            //Down: 40
+            //Space: 32
+
+            while (_running == true)
+            {
+                if (cki.Key.GetHashCode() == 38)//Key UP
+                {
+
+                }
+                else if (cki.Key.GetHashCode() == 40)//Key DOWN
+                {
+
+                }
+                else if (cki.Key.GetHashCode() == 32)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+    }
+}

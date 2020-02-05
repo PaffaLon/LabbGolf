fb4a9328b974284807300cc97ad3c6901216bca6
Tobias Olsson <tobiasbjornolsson@gmail.com>
2020-02-05 13:18:32 +0100

    Added the class "PlayerHandler". Added logic.

---
+++ LexiconLabb/GolfSimplyfied/Entities/Characters/Players/PlayerHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolfSimplyfied.Entities.Characters.Players
+{
+    internal class PlayerHandler
+    {
+        internal PlayerHandler()
+        {
+
+        }
+        //Construcor Helper Methods
+        private void SetDefaultValues()
+        {
+
+        }
+        //Class Methods
+        public void LoadPlayer()
+        {
+
+        }
+        public void GetPlayer()
+        {
+
+        }
+    }
+}

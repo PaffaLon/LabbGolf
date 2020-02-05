15231b5329c22ec94c97ad18d6f37cee38f279bf
Tobias Olsson <tobiasbjornolsson@gmail.com>
2020-02-05 22:11:23 +0100

    Added a new class.

---
+++ LexiconLabb/GolfSimplyfied/GameEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolfSimplyfied
+{
+    internal class GameEngine
+    {
+        public enum poco
+        {
+            GameControles,
+            RunLevel,
+            GetItem,
+            GetPlayer
+        }
+        internal GameEngine()
+        {
+
+        }
+        //Constructor Helper Methods
+        private void SetDefaultValues()
+        {
+
+        }
+        //Class Methods
+    }
+}
